651dfe59931d4e927fef7c049c7cbfa3cd0555a1
Ира Андреева <minuto_suerte@mail.ru>
2023-04-19 22:48:10 +0300

    ..

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,6 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
+DEBUG = False
+
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
