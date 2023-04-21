72be14890a3b09ac8f893a7655a882b4eca300b8
Ира Андреева <minuto_suerte@mail.ru>
2023-04-21 23:46:52 +0300

    ..

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,11 +8,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'web',
-]
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django_filters',
